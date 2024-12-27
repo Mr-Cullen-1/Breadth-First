da5b96243f5c79b245246b084636cf0590501da0
Jacob Cullen <mrcullen6256@gmail.com>
2024-12-27 22:18:33 +0500

    Breadth-first seraching algorithm

---
+++ main.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def search(start_node, target='elon musk'):
+    search_queue = deque()
+    search_queue += graph[start_node]
+    searched = set()
+    
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person == target:
+                print(f"{target} has been found!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.add(person)
+    return False
+
+
+if __name__ == '__main__':
+    graph = {
+        'siz':['ali', 'vali', 'tohir'],
+        'ali':['aziza', 'olim'],
+        'vali':['botir', 'ziyoda'],
+        'tohir':['elon musk', 'mohir'],
+        'olim':[], 
+        'aziza':[], 
+        'botir':[],
+        'ziyoda':['aziza'],
+        'elon musk':[],
+        'mohir':[]
+    }
+    search('siz')
